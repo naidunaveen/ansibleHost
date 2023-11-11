ac158d8ed9b27f7e667aedce2a246896b06a3115
Ubuntu <ubuntu@ip-172-31-25-30.eu-north-1.compute.internal>
2023-11-11 09:37:11 +0000

    host files

---
+++ plus.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+m=20
+n=15
+p=`expr $m + $n`
+echo "addition of m and n is $p"
+echo "$p"
---
+++ shellexe.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+a=9
+b=8
+c=`expr $a - $b`
+echo "subtraction of a and b is $c"
+echo "$c"
